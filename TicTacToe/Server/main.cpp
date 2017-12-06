ab82b7be8ccc6e051e39daf25edafddcaff791d1
Killua <646775589@qq.com>
2017-12-06 20:57:42 +0800

    Create main.cpp

---
+++ TicTacToe/Server/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  TicTacToeServer
+//
+//  Created by Tusko on 2017/4/9.
+//  Copyright © 2017年 Tusko. All rights reserved.
+//
+
+#include <iostream>
+#include "Board.hpp"
+
+
+int main(int argc, const char * argv[]) {
+ 
+    TTTServer TTTserv ;
+    return 0;
+}
+
+
+
