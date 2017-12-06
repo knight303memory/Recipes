458cdd6cbeeee0a13342b0a9dc6d4e7466902135
Killua <646775589@qq.com>
2017-12-06 20:59:00 +0800

    Create main.cpp

---
+++ TicTacToe/Client/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  TicTacToe_Client
+//
+//  Created by Tusko on 2017/4/9.
+//  Copyright © 2017年 Tusko. All rights reserved.
+//
+
+#include <iostream>
+
+#include "Board.hpp"
+
+int main(int argc, const char * argv[]) {
+   
+    
+    TTTClient TTTclnt ;
+    
+    return 0;
+}
